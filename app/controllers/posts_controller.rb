a2b06325229f3f609cf00b983ac1e0872897840a
Alex Liang <u9211011@gmail.com>
2016-04-22 09:43:19 +0800

    Add destroy action for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :find_params, only: [:show, :edit, :update]
+  before_action :find_params, only: [:show, :edit, :update, :destroy]
 
   def index    
     @posts = Post.all
@@ -31,6 +31,12 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @post.destroy
+
+    redirect_to posts_path
   end
 
   private
