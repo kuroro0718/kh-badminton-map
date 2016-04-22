0cc555c422b2a49cbb23c91133aa99a8d0e4098d
Alex Liang <u9211011@gmail.com>
2016-04-22 15:51:40 +0800

    Add helper to calculate next badminton meetup

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,16 @@
 module PostsHelper
+  def calculate_date(day_offset)
+    meetup_day = day_offset
+    current_day = Time.now.wday   
+    next_date = DateTime.now + (meetup_day - current_day).days      
+    
+    if meetup_day < current_day 
+      next_date += 7.days  # Next week
+    end
+    next_date.strftime('%Y/%m/%d')
+  end
+
+  def next_meetup_date(day_offset)
+    calculate_date(day_offset) + ' (' + I18n.t(:"date.day_names")[day_offset] + ')'
+  end
 end
