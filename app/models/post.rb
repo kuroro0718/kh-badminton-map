09b82b33d532a7b04b4ff96d50f7bed4c88509fa
Alex Liang <u9211011@gmail.com>
2016-04-26 11:11:14 +0800

    Modify navbar and index page style

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   geocoded_by :address
   after_validation :geocode
 
-  has_attached_file :image, styles: { medium: "400x400>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "400x400>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   belongs_to :user
