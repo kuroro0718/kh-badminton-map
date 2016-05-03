1da3c4ddb83a3081284f84409a4c00d3835ca284
Alex Liang <u9211011@gmail.com>
2016-05-03 16:21:06 +0800

    Only use S3 route in production

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,5 @@
-Paperclip::Attachment.default_options[:url] = 'kaohsiung-badminton-map.s3.amazonaws.com'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-1.amazonaws.com'
+if Rails.env.production?
+  Paperclip::Attachment.default_options[:url] = 'kaohsiung-badminton-map.s3.amazonaws.com'
+  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+  Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-1.amazonaws.com'
+end
